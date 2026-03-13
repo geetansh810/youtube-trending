5109fb6f54b81214b09d4069bbc3fad992be1cd0
Geetansh Agrawal <agrawalgeetansh810@gmail.com>
2026-03-14 04:50:41 +0530

    fix base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: "https://geetansh810.github.io/youtube-trending",
-  base: "/youtube-trending",
+  base: "/youtube-trending/",
 });
