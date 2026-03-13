f156b629647ba2a35f16d1abbfb10e59eb2859c7
Geetansh Agrawal <agrawalgeetansh810@gmail.com>
2026-03-14 04:45:24 +0530

    rebuild

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: "https://geetansh810.github.io",
+  site: "https://geetansh810.github.io/youtube-trending",
   base: "/youtube-trending",
 });
