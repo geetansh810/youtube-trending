e87577a8b675f910910755f602cfb98a75d71bf8
Geetansh Agrawal <agrawalgeetansh810@gmail.com>
2026-03-14 05:19:34 +0530

    fix base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   output: 'static',
   site: "https://geetansh810.github.io",
+  base: '/youtube-trending',
   vite: {
     define: {
       'import.meta.env.PUBLIC_YOUTUBE_API_KEY': JSON.stringify(process.env.PUBLIC_YOUTUBE_API_KEY || ''),
